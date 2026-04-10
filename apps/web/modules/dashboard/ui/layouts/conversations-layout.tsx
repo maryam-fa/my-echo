054c292e933376b4cba0226768eb313cae394ad6
maryam-fa <damnitxmaryam@gmail.com>
2026-04-10 04:57:42 -0700

    18: dashboard-chat

--- apps/web/modules/dashboard/ui/layouts/conversations-layout.tsx
+++ apps/web/modules/dashboard/ui/layouts/conversations-layout.tsx
@@ -11,13 +11,15 @@ export const ConversationsLayout = ({
 }: { children: React.ReactNode; }) => {
   return (
     // @ts-ignore
-    <ResizablePanelGroup className="h-full flex-1 w-full" direction="horizontal" customstoragekey="conversations-v2">
-      <ResizablePanel defaultSize={300} maxSize={500} minSize={300} >
+    <ResizablePanelGroup className="h-full flex-1 w-full overflow-hidden" direction="horizontal" customstoragekey="conversations-v2">
+      <ResizablePanel defaultSize={300} maxSize={500} minSize={300} className=" overflow-y-auto" >
         <ConversationsPanel />
       </ResizablePanel>
       <ResizableHandle withHandle/>
-      <ResizablePanel className="h-full" defaultSize={70}>
+      <ResizablePanel className="relative flex flex-col h-full" defaultSize={70}>
+        <div className="flex-1 min-h-0 overflow-y-auto">
         {children}
+        </div>
       </ResizablePanel>
     </ResizablePanelGroup>
   );
---
+++ packages/backend/convex/private/messages.ts
@@ -0,0 +1,117 @@
+import { v } from "convex/values";
+import { mutation, query } from "../_generated/server";
+import { components, internal } from "../_generated/api";
+import { ConvexError } from "convex/values";
+import { supportAgent } from "../system/ai/agents/supportAgent";
+import { paginationOptsValidator } from "convex/server";
+import { saveMessage } from "@convex-dev/agent";
+export const create = mutation({
+  args: {
+    prompt: v.string(),
+    conversationId:v.id("conversations"),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (identity === null) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "Identity not found",
+      });
+    }
+    const orgId = identity.orgId as string;
+    if (!orgId) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "Organization not found",
+      });
+    }
+
+    // 2. Fetch the conversation by thread ID
+    const conversation = await ctx.db.get(args.conversationId);
+    if (!conversation) {
+      throw new ConvexError({
+        code: "NOT_FOUND",
+        message: "Conversation not found",
+      });
+    }
+
+    if (conversation.organizationId !== orgId) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "Invalid Organization ID",
+      });
+    }
+
+    // 3. Check if conversation is already resolved
+    if (conversation.status === "resolved") {
+      throw new ConvexError({
+        code: "BAD_REQUEST",
+        message: "Conversation resolved",
+      });
+    }
+
+    await saveMessage(ctx, components.agent, {
+      threadId: conversation.threadId,
+      agentName: identity.familyName,
+      message: {
+        role: "assistant",
+        content: args.prompt,
+      },
+    });
+  },
+});
+
+
+export const getMany = query({
+   args: {
+    threadId: v.string(),
+    paginationOpts: v.optional(paginationOptsValidator),
+   },
+   handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (identity === null) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "Identity not found",
+      });
+    }
+
+    const conversation = await ctx.db
+    .query("conversations")
+    .withIndex("by_thread_id", (q) => q.eq("threadId", args.threadId))
+    .unique();
+
+    if (!conversation) {
+      throw new ConvexError({
+        code: "NOT_FOUND",
+        message: "Conversation not found",
+      });
+    }
+    const orgId = identity.orgId as string;
+
+    if (conversation.organizationId !== orgId) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "Invalid Organization ID",
+      });
+    }
+
+
+    if (!orgId) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "Organization not found",
+      });
+    }
+     
+       const paginated = await supportAgent.listMessages(ctx, {
+          threadId: args.threadId,
+          paginationOpts: args.paginationOpts!,
+
+        });
+
+       return paginated;
+    },
+});
